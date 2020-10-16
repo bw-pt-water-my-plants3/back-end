9d30c217a2fe3d6e69cc5e724938e8b388276e55
dustin <dustnclaytiner@gmail.com>
2020-10-15 20:55:16 -0500

    merged branches

---
+++ data/migrations/20201015183400_create-plants.js
@@ -0,0 +1,30 @@
+
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('User', user => {
+            user.increments();
+            user.string('username', 128).unique().notNullable();
+            user.string('phoneNumber', 128).notNullable();
+            user.string('password', 128).notNullable();
+        })
+        .createTable('Plant', plant => {
+            plant.increments();
+            plant.string('nickname', 128);
+            plant.string('species', 128);
+            plant.string('h2oFrequency', 128);
+            plant.string('image').nullable();
+            plant.integer('user_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('User')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+        });
+};
+
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExists('Plant')
+        .dropTableIfExists('User');
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const AuthRouter = require('./auth/authRouter');
+const plantRouter = require('./plant/plant-router');
 const cors = require('cors');
 const errorHandler = require('./auth/ErrorHandler')
 const authenticate = require('./auth/authenticate-middleware')
@@ -12,7 +13,11 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 server.use('/auth', AuthRouter);
+<<<<<<< HEAD
 server.use('/user',authenticate, UserRouter);
+=======
+server.use('/plant', plantRouter);
+>>>>>>> f937fcde3627e1a4a7c9cfab595d50bd85f0eef5
 
 server.get('/', (req,res) => {
     res.status(200).json({message:'Welcome to the Plants Backend'})
