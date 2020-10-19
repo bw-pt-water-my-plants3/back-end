055f259f157bab10d5d5baa8ab5e1e7c879860bb
dustin <dustnclaytiner@gmail.com>
2020-10-18 20:47:57 -0500

    added more test users

--- data/seeds/002user.js
+++ data/seeds/002user.js
@@ -4,7 +4,11 @@ exports.seed = function(knex) {
   return knex('User').del()
     .then(function () {
       return knex('User').insert([
-        {username:'test', password:'$2a$08$66.rWYhq5CkVPLdK03XhBuczqrOqEt623qSjAjGhikKMPxW4QGUAO',phoneNumber:12345678910},
+        {username:'test1', password:'$2a$08$66.rWYhq5CkVPLdK03XhBuczqrOqEt623qSjAjGhikKMPxW4QGUAO',phoneNumber:12345678910},
+        {username:'test2', password:'$2a$08$66.rWYhq5CkVPLdK03XhBuczqrOqEt623qSjAjGhikKMPxW4QGUAO',phoneNumber:12345678910},
+        {username:'test3', password:'$2a$08$66.rWYhq5CkVPLdK03XhBuczqrOqEt623qSjAjGhikKMPxW4QGUAO',phoneNumber:12345678910},
+        {username:'test4', password:'$2a$08$66.rWYhq5CkVPLdK03XhBuczqrOqEt623qSjAjGhikKMPxW4QGUAO',phoneNumber:12345678910},
+        {username:'test5', password:'$2a$08$66.rWYhq5CkVPLdK03XhBuczqrOqEt623qSjAjGhikKMPxW4QGUAO',phoneNumber:12345678910}
       ]);
     });
 };
