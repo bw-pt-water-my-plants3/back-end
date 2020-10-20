7022a3ea128633a9e14c9d89478a2ef237fffdf6
JenVest2020 <jenvest2019@gmail.com>
2020-10-19 20:42:45 -0500

    revised server file to show user list to know deployment is good.

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const db = require('../user/userModel.js');
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/authRouter.js');
@@ -11,6 +12,17 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    db.find()
+        .then((users) => {
+            res.status(200).json(users)
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+
+})
 
 server.use('/api/auth', authRouter);
 server.use('/api/plants', authenticate, plantsRouter);
