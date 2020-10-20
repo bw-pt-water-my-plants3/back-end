37a58cb5ca6e757f1c99bcd0135b24b976d3caec
JenVest2020 <jenvest2019@gmail.com>
2020-10-19 20:14:12 -0500

    revised seed user file for production

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,8 +1,8 @@
 
 exports.seed = function (knex, Promise) {
   return knex('users').insert([
-    { username: 'PoisonIvy', phone_number: '(555)123-4567', password: 'Itchy1' },
-    { username: 'Jenny', phone_number: '(555)867-5309', password: 'Music80' },
-    { username: 'Audrey2', phone_number: '(212)555-6789', password: 'FeedMe86' },
+    { username: 'PoisonIvy', phone_number: '5551234567', password: 'Itchy1' },
+    { username: 'Jenny', phone_number: '5558675309', password: 'Music80' },
+    { username: 'Audrey2', phone_number: '2125556789', password: 'FeedMe86' },
   ]);
 };
