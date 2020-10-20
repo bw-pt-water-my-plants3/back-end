dd8fd2bb8dd989323b1f991094efe2996e4e304a
JenVest2020 <jenvest2019@gmail.com>
2020-10-19 20:07:31 -0500

    installed pg as dependency and added to my knexfile

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
     seeds: { directory: './data/seeds' },
   },
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
