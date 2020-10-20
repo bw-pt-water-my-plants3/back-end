0d0260daa1c8c6db1b1587ce5d94353f41548a7d
dustin <dustnclaytiner@gmail.com>
2020-10-19 20:30:04 -0500

    modified first test a little bit

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,11 +2,12 @@ const request = require('supertest');
 const server = require('./server')
 const db = require('../data/connection');
 
-
 describe('server.js', () => {
-    it('should return welcome message', async () => {
-        const res = await request(server).get('/')
-        expect(res.type).toBe('application/json')
-        expect(res.text).toBe('{\"message\":"Welcome to the Plants Backend\"}')
-    })
-}) 
+    describe('Get to base URL', () => {
+        it('should return welcome message', async () => {
+            const res = await request(server).get('/')
+            expect(res.type).toBe('application/json')
+            expect(res.text).toBe('{\"message\":"Welcome to the Plants Backend\"}')
+        })
+    }) 
+})
