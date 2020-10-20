9ebc9643dfeb43d6308dc651b5e2143973ab95b2
JenVest2020 <jenvest2019@gmail.com>
2020-10-19 20:21:18 -0500

    revised migration user phone to bigIntiger for deployment

--- data/migrations/20201014132916_createtables.js
+++ data/migrations/20201014132916_createtables.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
         .createTable('users', tbl => {
             tbl.increments();
             tbl.string('username', 128).notNullable().unique();
-            tbl.integer('phone_number');
+            tbl.bigInteger('phone_number');
             tbl.varchar('password', 128).notNullable();
         })
         .createTable('plants', tbl => {
