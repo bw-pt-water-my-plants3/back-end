ab5ef9561e63d776e8257504e2ff795a643707bd
JenVest2020 <jenvest2019@gmail.com>
2020-10-19 19:58:59 -0500

    put knex file client back to postgresql for deployment

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
     seeds: { directory: './data/seeds' },
   },
   production: {
-    client: 'pg',
+    client: 'postgresql',
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
