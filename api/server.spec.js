a05abe58f99442bbae6c20045c41414b7b8e982f
JenVest2020 <jenvest2019@gmail.com>
2020-10-22 14:23:35 -0500

    working on issues with plantsRouter test code again

--- api/server.spec.js
+++ api/server.spec.js
@@ -14,3 +14,54 @@ describe('server.js', () => {
         expect(res.type).toBe('application/json');
     });
 });
+
+let token;
+
+beforeAll((done) => {
+    request(server)
+        .post('/register')
+        .send({
+            username: 'new user1', phone_number: '9138312137',
+            password: 'testPatch'
+        })
+        .end((err, response) => {
+            token = response.body.token;
+            done();
+        });
+});
+
+describe('Get /api/plants', () => {
+    it('should return a 200 with an authorized user', () => {
+        return request(server)
+            .get('/api/plants')
+            .then((response) => {
+                expect(response.status).toBe(200);
+            });
+    });
+    it('should return JSON with authorized user', () => {
+        return request(server)
+            .get('/api/plants')
+            .then((response) => {
+                expect(response.type).toBe('application/json');
+            });
+    });
+});
+
+describe('Post /api/plants', () => {
+    it('should return a 201 with created plant info', () => {
+        return request(server)
+            .post('/api/plants')
+            .send({ nickname: 'Foxglove', species: 'Digitalis purpurea' })
+            .then((response) => {
+                expect(response.status).toBe(201);
+            });
+    });
+    it('should return JSON with created plant info', () => {
+        return request(server)
+            .post('/api/plants')
+            .send({ nickname: 'Foxglove', species: 'Digitalis purpurea' })
+            .then((response) => {
+                expect(response.type).toBe('application/json');
+            });
+    });
+})
